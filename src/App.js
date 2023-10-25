9522996d89192acd596b022067d6a4fd1aab4473
oqoqoh <oqoq59@gmail.com>
2023-10-25 23:57:31 +0900

    ➕ QueryClientProvider  추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
 import MainProducts from './components/MainProducts';
+import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from 'react-query';
 
 export default function App() {
-    return <MainProducts />;
+    return (
+        // Provide the client to your App
+        <QueryClientProvider client={queryClient}>
+            <MainProducts />
+        </QueryClientProvider>
+    );
 }
