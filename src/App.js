18384bee5a5097c637753a06c0b33bcd1ebb643e
oqoqoh <oqoq59@gmail.com>
2023-10-25 23:59:55 +0900

    useQuery fetch 조회 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import MainProducts from './components/MainProducts';
-import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 export default function App() {
     return (
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,31 +1,39 @@
 import React, { useState } from 'react';
-import useProducts from '../hooks/use-products';
+import { useQuery } from 'react-query';
 
 export default function Products() {
-  const [checked, setChecked] = useState(false);
-  const [loading, error, products] = useProducts({ salesOnly: checked });
-  const handleChange = () => setChecked((prev) => !prev);
+    const [checked, setChecked] = useState(false);
+    const {
+        isLoading,
+        error,
+        data: products,
+    } = useQuery(['products'], async () => {
+        return fetch(`data/products.json`).then((res) => res.json());
+    });
 
-  if (loading) return <p>Loading...</p>;
+    //const [loading, error, products] = useProducts({ salesOnly: checked });
+    const handleChange = () => setChecked((prev) => !prev);
 
-  if (error) return <p>{error}</p>;
+    if (isLoading) return <p>Loading...</p>;
 
-  return (
-    <>
-      <label>
-        <input type='checkbox' checked={checked} onChange={handleChange} />
-        Show Only 🔥 Sale
-      </label>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <article>
-              <h3>{product.name}</h3>
-              <p>{product.price}</p>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+    if (error) return <p>{error}</p>;
+
+    return (
+        <>
+            <label>
+                <input type="checkbox" checked={checked} onChange={handleChange} />
+                Show Only 🔥 Sale
+            </label>
+            <ul>
+                {products.map((product) => (
+                    <li key={product.id}>
+                        <article>
+                            <h3>{product.name}</h3>
+                            <p>{product.price}</p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
