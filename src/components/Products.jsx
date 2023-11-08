93ad615d62b754f086f90b83f25f187a9c115e54
oqoqoh <oqoq59@gmail.com>
2023-11-08 21:57:55 +0900

    🔊 fetching 확인

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,39 +1,41 @@
-import React, { useState } from 'react';
-import { useQuery } from 'react-query';
+import React, { useState } from "react";
+import { useQuery } from "react-query";
 
 export default function Products() {
-    const [checked, setChecked] = useState(false);
-    const {
-        isLoading,
-        error,
-        data: products,
-    } = useQuery(['products', checked], async () => {
-        return fetch(`data/${checked ? 'sale_' : ''}products.json`).then((res) => res.json());
-    });
-
-    //const [loading, error, products] = useProducts({ salesOnly: checked });
-    const handleChange = () => setChecked((prev) => !prev);
-
-    if (isLoading) return <p>Loading...</p>;
-
-    if (error) return <p>{error}</p>;
-
-    return (
-        <>
-            <label>
-                <input type="checkbox" checked={checked} onChange={handleChange} />
-                Show Only 🔥 Sale
-            </label>
-            <ul>
-                {products.map((product) => (
-                    <li key={product.id}>
-                        <article>
-                            <h3>{product.name}</h3>
-                            <p>{product.price}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </>
+  const [checked, setChecked] = useState(false);
+  const {
+    isLoading,
+    error,
+    data: products,
+  } = useQuery(["products", checked], async () => {
+    console.log("fetching...");
+    return fetch(`data/${checked ? "sale_" : ""}products.json`).then((res) =>
+      res.json()
     );
+  });
+
+  const handleChange = () => setChecked((prev) => !prev);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    <>
+      <label>
+        <input type="checkbox" checked={checked} onChange={handleChange} />
+        Show Only 🔥 Sale
+      </label>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
